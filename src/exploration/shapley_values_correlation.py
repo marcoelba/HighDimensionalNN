f0e0ec9cc0c0eeb7632e7d9555dd1f52e3c7d4c7
Marco Molinari <marco.molinari@medisin.uio.no>
2025-12-10 13:57:51 +0100

    modified diagrams

--- src/exploration/shapley_values_correlation.py
+++ src/exploration/shapley_values_correlation.py
@@ -18,59 +18,37 @@ from src.utils import data_loading_wrappers
 
 # generate linear regression data
 n = 100
-p = 5
-
-# covarites with different var
-x1 = np.random.randn(n, 1)
-x2 = np.random.randn(n, 1) * 10
-x3 = np.random.randn(n, 1) * 0.1
-x4 = np.random.randn(n, 1) * 0.1
-x5 = np.random.randn(n, 1) * 0.1
-
-X = np.concatenate([x1, x2, x3, x4, x5], axis=1)
-
-y = X.dot(np.array([1., 1., 1., 1., 1.])) + np.random.randn(n) * 0.2
-y = y[..., None]
+p = 3
 
 # if correlated
 X = np.random.randn(n, p)
 cov_matrix = np.zeros([p, p])
-np.fill_diagonal(cov_matrix, [1., 2., 0.1, 0.1, 0.1])
+np.fill_diagonal(cov_matrix, 1.)
 # cor x1, x2
-cov_matrix[0, 1] = cov_matrix[1, 0] = 0.5
-# cor x3, x4, x5
-cov_matrix[2, 3] = cov_matrix[3, 2] = 0.05
-cov_matrix[2, 4] = cov_matrix[4, 2] = 0.04
-cov_matrix[3, 4] = cov_matrix[4, 3] = 0.04
+cov_matrix[0, 1] = cov_matrix[1, 0] = 0.98
 
 np.round(np.cov(X.dot(np.linalg.cholesky(cov_matrix).transpose()), rowvar=False), 3)
 np.round(np.corrcoef(X.dot(np.linalg.cholesky(cov_matrix).transpose()), rowvar=False), 3)
 
 X = X.dot(np.linalg.cholesky(cov_matrix).transpose())
-y = X.dot(np.array([1., 1., 1., 1., 1.])) + np.random.randn(n) * 0.2
+y = X.dot([1., 0., 1.]) + np.random.randn(n) * 0.2
 y = y[..., None]
 
 # ----------------------- OLS ------------------------
 np.linalg.inv(X.transpose().dot(X)).dot(X.transpose().dot(y))
 # ~[1., .1, .1]
 
-# now with feature standardization
-scaler = StandardScaler()
-
-X_std = scaler.fit_transform(X)
-np.linalg.inv(X_std.transpose().dot(X_std)).dot(X_std.transpose().dot(y))
-# ~[1., 0.1, 10.]
-
-
 # ----------------------- NN ------------------------
 class LinearModel(nn.Module):
     def __init__(self, input_dim: int):
         super(LinearModel, self).__init__()
-        self.fc1 = nn.Linear(input_dim, 5)
-        self.fc2 = nn.Linear(5, 1)
+        # self.fc1 = nn.Linear(input_dim, 5)
+        # self.fc2 = nn.Linear(5, 1)
+        self.fc1 = nn.Linear(input_dim, 1)
 
     def forward(self, batch):
-        return self.fc2(self.fc1(batch[0]))
+        # return self.fc2(self.fc1(batch[0]))
+        return self.fc1(batch[0])
 
     def loss(self, m_out, batch):
         # label prediction loss
@@ -79,8 +57,6 @@ class LinearModel(nn.Module):
 
 # get tensors
 X_tensor = torch.FloatTensor(X).to(torch.device("cpu"))
-# or
-X_tensor = torch.FloatTensor(X_std).to(torch.device("cpu"))
 
 y_tensor = torch.FloatTensor(y).to(torch.device("cpu"))
 data_tensor = [X_tensor, y_tensor]
@@ -94,7 +70,7 @@ model_nn = LinearModel(p).to(device)
 optimizer = optim.Adam(model_nn.parameters(), lr=1e-3)
 
 # Training Loop
-num_epochs = 5000
+num_epochs = 2000
 
 trainer = training_wrapper.Training(dataloader)
 trainer.training_loop(model_nn, optimizer, num_epochs)
@@ -138,36 +114,11 @@ explanation = shap.Explanation(
 )
 shap.plots.waterfall(explanation, show=True)
 
-sum_shap_patient = np.abs(shap_values).sum(axis=1)
-sum_shap_patient[np.argsort(sum_shap_patient)[::-1]]
-
-# isolate features with small variance
-small_std = np.std(shap_values, axis=0) < 0.5
-
-shap.summary_plot(
-    shap_values[:, small_std],
-    features=X_tensor[:, small_std],
-    show=True
-)
-
-row_id = 0
-explanation = shap.Explanation(
-    values=shap_values[row_id, small_std],
-    base_values=base_value,
-    data=X[row_id, small_std]
-)
-shap.plots.waterfall(explanation, show=True)
-
-
 # new samples - 3 different observations
-new_data = torch.tensor(np.array([[0., 0., 0.], [1., 0.1, -10.], [1., 1., -1.]]), dtype=torch.float32)
-
-# with correlation
-np.sqrt(cov_matrix)
 new_data = torch.tensor(np.array([
-    [0., 0., 0., 0., 0.],
-    [1., 3., 0.3, 0.25, 0.35],
-    [1., -3., 0.3, -0.3, 0.35]
+    [0., 0., 0.],
+    [1., 1.1, -1.],
+    [2., 1.9, 0.]
     ]),
     dtype=torch.float32
 )
@@ -186,19 +137,51 @@ explanation = shap.Explanation(
 shap.plots.waterfall(explanation, show=True)
 # one standard deviation corresponds to a change according to the estimated shapley value
 
-norm_shap_values = shap_values / shap_values_std
 
-row_id = 2
+# --------------------------------------------------------------------------
+# Using Kernel Shap, which does not account for correlation in the features
+# --------------------------------------------------------------------------
+
+# SHAP explanation
+def model_shap(x):
+    x = torch.Tensor(x)
+    pred = model_nn([x, x]).detach().numpy()
+    return pred
+
+model_shap(X)
+
+base_value = model_shap(X).mean()
+
+explainer = shap.KernelExplainer(model_shap, X)
+shap_values = explainer.shap_values(X)
+shap_values.shape
+shap_values = shap_values[..., -1]
+
+shap.summary_plot(
+    shap_values,
+    features=X,
+    show=True
+)
+shap_values_std = shap_values.std(axis=0)
+shap_values_std
+
+row_id = 0
 explanation = shap.Explanation(
-    values=norm_shap_values[row_id, :],
+    values=shap_values[row_id, :],
     base_values=base_value,
-    data=new_data[row_id, :].numpy()
+    data=X[row_id, :]
 )
 shap.plots.waterfall(explanation, show=True)
 
-np.abs(shap_values).mean(axis=0)
-np.abs(shap_values)[:, 0].mean()
-# split by sign
-col = 2
-shap_values[shap_values[:, col] > 0, col].mean()
-shap_values[shap_values[:, col] < 0, col].mean()
+
+shap_values = explainer.shap_values(new_data.numpy())
+shap_values.shape
+shap_values = shap_values[..., -1]
+
+row_id = 2
+explanation = shap.Explanation(
+    values=shap_values[row_id, :],
+    base_values=base_value,
+    data=new_data.numpy()[row_id, :]
+)
+shap.plots.waterfall(explanation, show=True)
